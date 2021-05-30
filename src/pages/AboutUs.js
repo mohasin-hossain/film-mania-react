d9c5a91396350e1561d7b19d4aa85322d9ed7df3
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2021-05-30 11:20:08 +0600

    About Section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+// Importing Components
+import AboutSection from "../components/AboutSection";
+
+const AboutUs = () => {
+  return (
+      <AboutSection />
+  );
+};
+
+export default AboutUs;
